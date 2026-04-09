f098883dcda8bf57e75e247554abeb1c6e0716f0
AllanTran90 <allan_sweden90@hotmail.com>
2026-04-09 11:04:33 +0200

    feat: add game loop and snake movement

--- src/snake-game/Program.cs
+++ src/snake-game/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 int width = 20;
 int height = 10;
@@ -6,18 +7,27 @@ int height = 10;
 int snakeX = 5;
 int snakeY = 5;
 
-for (int y = 0; y < height; y++)
+while (true)
 {
-    for (int x = 0; x < width; x++)
+    Console.Clear();
+
+    for (int y = 0; y < height; y++)
     {
-        if (x == snakeX && y == snakeY)
+        for (int x = 0; x < width; x++)
         {
-            Console.Write("O");
+            if (x == snakeX && y == snakeY)
+            {
+                Console.Write("O");
+            }
+            else
+            {
+                Console.Write(".");
+            }
         }
-        else
-        {
-            Console.Write(".");
-        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
+
+    snakeX++;
+
+    Thread.Sleep(200);
 }
