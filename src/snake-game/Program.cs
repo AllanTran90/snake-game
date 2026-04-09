958f2dd13dba36778b777277c4d2c7aecb4e9756
AllanTran90 <allan_sweden90@hotmail.com>
2026-04-09 11:21:34 +0200

    feat: added keyboard input and snake control

--- src/snake-game/Program.cs
+++ src/snake-game/Program.cs
@@ -6,6 +6,9 @@ using System.Linq;
 int width = 20;
 int height = 10;
 
+//direction
+string direction = "RIGHT";
+
 // snake body (head first)
 List<(int x, int y)> snake = new List<(int, int)>
 {
@@ -32,18 +35,47 @@ while (true)
                 Console.Write(".");
             }
         }
+
         Console.WriteLine();
     }
 
-    // movement (right)
-    var head = snake[0];
-    var newHead = (head.x + 1, head.y);
+    //input
+    if (Console.KeyAvailable)
+    {
+        var key = Console.ReadKey(true).Key;
 
-    // add new head
+        switch (key)
+        {
+            case ConsoleKey.UpArrow:
+                direction = "UP";
+                break;
+            case ConsoleKey.DownArrow:
+                direction = "DOWN";
+                break;
+            case ConsoleKey.LeftArrow:
+                direction = "LEFT";
+                break;
+            case ConsoleKey.RightArrow:
+                direction = "RIGHT";
+                break;
+        }
+    }
+
+    var head = snake[0];
+
+    (int x, int y) newHead = direction switch
+    {
+        "UP" => (head.x, head.y - 1),
+        "DOWN" => (head.x, head.y + 1),
+        "LEFT" => (head.x - 1, head.y),
+        _ => (head.x + 1, head.y)
+    };
+
+    // lägg till nytt huvud
     snake.Insert(0, newHead);
 
-    // remove tail
+    // ta bort svans
     snake.RemoveAt(snake.Count - 1);
 
-    Thread.Sleep(200);
+    Thread.Sleep(150);
 }
