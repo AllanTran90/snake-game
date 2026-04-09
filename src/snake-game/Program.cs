5e82a573441cf0efde02e0cbf9730a583fe84dc6
AllanTran90 <allan_sweden90@hotmail.com>
2026-04-09 10:59:59 +0200

    feat: added grid

--- src/snake-game/Program.cs
+++ src/snake-game/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+int width = 20;
+int height = 10;
+
+int snakeX = 5;
+int snakeY = 5;
+
+for (int y = 0; y < height; y++)
+{
+    for (int x = 0; x < width; x++)
+    {
+        if (x == snakeX && y == snakeY)
+        {
+            Console.Write("O");
+        }
+        else
+        {
+            Console.Write(".");
+        }
+    }
+    Console.WriteLine();
+}
